ddb49b14303f80932cc53242636af8d6312f56ab
sebastian beltran zapata <sebastian199502@hotmail.com>
2023-08-31 16:19:17 -0500

    Angular 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
 import { ContadorComponent } from './contador/contador.component';
+import { FinazasComponent } from './finazas/finazas.component';
+import { PruebaComponent } from './prueba/prueba.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContadorComponent
+    ContadorComponent,
+    FinazasComponent,
+    PruebaComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/finazas/finazas.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-finazas',
+  templateUrl: './finazas.component.html',
+  styleUrls: ['./finazas.component.css']
+})
+export class FinazasComponent {
+
+}
