ba19d6e266bfb6eed610063b5329a63e3454091f
sebastian beltran zapata <sebastian199502@hotmail.com>
2023-09-01 16:50:55 -0500

    interpolacion angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,5 +15,5 @@ import { Component } from '@angular/core';
 //   `]
 })
 export class AppComponent {
-  title = 'curso_angular';
+  title = 'Curso Angular';
 }
--- src/app/finazas/finazas.component.ts
+++ src/app/finazas/finazas.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { persona } from '../persona';
 
 @Component({
   selector: 'app-finazas',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./finazas.component.css']
 })
 export class FinazasComponent {
-
+  nombre:any = 'pepito perez';
+  persona:persona = {
+    nombre: 'prueba',
+    edad: 30
+  }
 }
