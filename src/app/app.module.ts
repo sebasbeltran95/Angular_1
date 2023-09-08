e4860584cf687372f9acc66da1c8c23138c4f7a6
sebastian beltran zapata <sebastian199502@hotmail.com>
2023-09-08 14:41:49 -0500

    formularios reactivos angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,8 @@ import { BuclesComponent } from './bucles/bucles.component';
 import { SwitchComponent } from './switch/switch.component';
 import { Formulario2Component } from './formulario2/formulario2.component';
 
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Formulario3Component } from './formulario3/formulario3.component';
 
 @NgModule({
   declarations: [
@@ -24,11 +25,13 @@ import { FormsModule } from '@angular/forms';
     FormularioComponent,
     BuclesComponent,
     SwitchComponent,
-    Formulario2Component
+    Formulario2Component,
+    Formulario3Component
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/formulario3/formulario3.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-formulario3',
+  templateUrl: './formulario3.component.html',
+  styleUrls: ['./formulario3.component.css']
+})
+export class Formulario3Component {
+  // name = new FormControl('sebastian');
+  // email = new FormControl('sebastian@hotmail.com');
+  name = new FormControl('', Validators.required);
+  email = new FormControl('',Validators.required);
+  
+}
